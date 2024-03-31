994221ad326f9eac56d6589b04a5b004b170835e
TranMinh <tranphanquangminh2306@gmail.com>
2024-03-31 14:17:49 +0700

    add css dashboard, login

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -1,43 +1,37 @@
 @extends('dashboard')
 
 @section('content')
-    <main class="login-form">
-        <div class="cotainer">
-            <div class="row justify-content-center">
-                <div class="col-md-4">
-                    <div class="card">
-                        <h3 class="card-header text-center">Login</h3>
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('user.authUser') }}">
-                                @csrf
-                                <div class="form-group mb-3">
-                                    <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
-                                           autofocus>
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="d-grid mx-auto">
-                                    <button type="submit" class="btn btn-dark btn-block">Signin</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+<div class="login-card">
+    <h3 class="login-header">Màn hình đăng nhập</h3>
+    <div class="card-body">
+        <form method="POST" action="{{ route('user.authUser') }}">
+            @csrf
+            <div class="input">
+                <label for="email">Email</label>
+                <input type="text" placeholder="Email" id="email" class="form-control" name="email" required autofocus>
+                @if ($errors->has('email'))
+                <span class="text-danger">{{ $errors->first('email') }}</span>
+                @endif
+            </div>
+            <div class="input">
+                Password <input type="password" placeholder="Password" id="password" class="form-control"
+                    name="password" required>
+                @if ($errors->has('password'))
+                <span class="text-danger">{{ $errors->first('password') }}</span>
+                @endif
+            </div>
+            <div class="form-group mb-3">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember"> Remember Me
+                    </label>
                 </div>
             </div>
-        </div>
-    </main>
+            <div class="d-grid mx-auto">
+                <button type="submit" class="btn btn-dark btn-block">Signin</button>
+            </div>
+        </form>
+    </div>
+</div>
+
 @endsection
